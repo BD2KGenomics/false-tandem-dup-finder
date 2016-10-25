42feb3843b4a916f06b8a28fb13b1f402b21da17
Brandon Saint-John <bsaintjo@ucsc.edu>
2016-10-25 10:58:52 -0700

    Avoid writing results to file, instead to stdout

--- findScaffoldGapDups.py
+++ findScaffoldGapDups.py
@@ -5,7 +5,7 @@ import re
 import os
 # import subprocess
 from argparse import ArgumentParser
-from sonLib.bioio import fastaRead, system, getTempFile
+from sonLib.bioio import fastaRead, popenCatch, getTempFile
 
 
 def alignWithBlat(seq1, seq2):
@@ -15,7 +15,6 @@ def alignWithBlat(seq1, seq2):
     Returns size and % ID of the alignment."""
     seq1Path = getTempFile()
     seq2Path = getTempFile()
-    outPath = getTempFile()
     try:
         with open(seq1Path, 'w') as seq1File:
             with open(seq2Path, 'w') as seq2File:
@@ -23,45 +22,37 @@ def alignWithBlat(seq1, seq2):
                 seq2File.write('>\n' + seq2)
 
         # Run blat command
-        # system("blat %s %s -q=dna -tileSize=12 -stepSize=4 -minIdentity=95"
-        #            " -repMatch=10 -noHead %s" % (seq1Path, seq2Path, outPath))
-        cmd = "blat {} {} -q=dna -tileSize=12 -stepSize=4 -minIdentity=95" \
-              " -repMatch=10 -noHead -fastMap {}".format(seq1Path, seq2Path, outPath)
-        # with open(os.devnull, 'w') as devnull:
-        #     subprocess.call(cmd, shell=True, bufsize=-1, stdout=devnull)
-        system(cmd)
-
+        cmd = "blat {} {} -q=dna -tileSize=12 -stepSize=4 -minIdentity=95 " \
+              "-repMatch=10 -noHead -fastMap stdout".format(seq1Path, seq2Path)
+        output = popenCatch(cmd)
 
         # There may be multiple alignments, but there can be at most one
         # that fits our requirements (starts at 0 in seq1, ends at
         # len(seq2))
 
-        with open(outPath, 'r') as outFile:
-            for line in outFile:
-                line = line.rstrip()
-                # print(line)
-                if len(line) == 0:
-                    continue
-                stats = line.split('\t')
-                # Starts are 0-based inclusive, ends are 0-based exclusive
-                seq2Start = int(stats[11])
-                seq1End = int(stats[16])
-                if seq1End != len(seq1) or seq2Start != 0:
-                    # Invalid.
-                    continue
-                # If we get here, this is the one possible alignment, so
-                # it's safe to return without checking the rest
-                # return seq2End - seq2Start, identity
+        for line in output.split('\n'):
+            line = line.rstrip()
+            if len(line) == 0:
+                continue
+            stats = line.split('\t')
+            # Starts are 0-based inclusive, ends are 0-based exclusive
+            seq2Start = int(stats[11])
+            seq1End = int(stats[16])
+            if seq1End != len(seq1) or seq2Start != 0:
+                # Invalid.
+                continue
+            # If we get here, this is the one possible alignment, so
+            # it's safe to return without checking the rest
+            # return seq2End - seq2Start, identity
 
-                dupPct = float(stats[0]) / (float(stats[0]) + float(stats[1]))
-                dupPct *= 100
-                # TODO num of mismatches or size of duplication?
-                return stats[0], dupPct
-            return 0, 0.0
+            dupPct = float(stats[0]) / (float(stats[0]) + float(stats[1]))
+            dupPct *= 100
+            # TODO num of mismatches or size of duplication?
+            return stats[0], dupPct
+        return 0, 0.0
     finally:
         os.remove(seq1Path)
         os.remove(seq2Path)
-        os.remove(outPath)
 
 
 def findGaps(sequence):
