e201b6af06b57cc8af54a63f79ea86886f8cc7e6
Brandon Saint-John <bsaintjo@ucsc.edu>
2016-11-16 10:07:28 -0800

    mv remove_overlap, assembly scaffold stats, etc.
    - Add .py to remove_overlap so the functions and FalseDup can
      be reused in dupsToBed.py
    - dupsToBed.py: converts a dups file to a bed file
    - compareAssemblyScaffolds: given a 2bit file and its corresponding dups
    file it will return the total number of scaffolds, number of false
    scaffold dups, and the percentage

---
+++ compareAssemblyScaffolds.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+"""For a given assembly in twobit file format, as well its corresponding
+duplications file, returns the assemblies total number of scaffold gaps,
+the number of scaffold gaps found to be apart of a duplication, and a
+percentage based on the ratio of these two values.
+"""
+from argparse import ArgumentParser
+from twobitreader import TwoBitFile
+from findScaffoldGapDups import findGaps
+
+
+def main():
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('twobit', help='twobit assembly file')
+    parser.add_argument('dups', help='corresponding dups file')
+    opts = parser.parse_args()
+
+    with open(opts.dups, 'r') as dupsf:
+        next(dupsf)  # Header
+        dupsScaffolds = sum(1 for line in dupsf)
+
+    total = 0
+    for sequence in TwoBitFile(opts.twobit).itervalues():
+        total += sum(1 for _, _ in findGaps(str(sequence)))
+
+    base = opts.dups.split('.')[0]
+    print("{}\t{}\t{}\t{:.2f}".format(base, total, dupsScaffolds,
+          float(dupsScaffolds) / total))
+
+
+if __name__ == '__main__':
+    main()
---
+++ dupsToBed.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+"""Converts a given dups file from findScaffoldGaps into a bed file.
+"""
+import remove_overlap
+from argparse import ArgumentParser
+
+
+def main():
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('dups', help='dups file')
+    opts = parser.parse_args()
+
+    duplication = remove_overlap.parse_dups_file(opts.dups)
+    for name, dups in duplication.items():
+        for dup in dups:
+            aboveStart = dup.gapStart - dup.dupSize
+            behindEnd = dup.gapEnd + dup.dupSize
+            aboveGap = "{} {} {}".format(name, aboveStart, dup.gapStart)
+            belowGap = "{} {} {}".format(name, dup.gapEnd, behindEnd)
+            print(aboveGap)
+            print(belowGap)
+
+
+if __name__ == '__main__':
+    main()
